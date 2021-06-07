3b853d9ee83fa7fbe47d67519a1a0a7a8916a885
ragman <raghuveergummadi9@gmail.com>
2021-06-06 23:19:58 -0400

    added base template, added file for upload view

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -27,7 +27,9 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-    from . import auth
-    app.register_blueprint(auth.bp)
+
+    from . import upload
+    app.register_blueprint(upload.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
